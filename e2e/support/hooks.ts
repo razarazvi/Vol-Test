3851cd0530a8c9619144820719b35c866a93fcc9
student <student@safebear.co.uk>
2019-10-03 13:47:47 +0100

    chnages to hooks.ts file

--- e2e/support/hooks.ts
+++ e2e/support/hooks.ts
@@ -6,3 +6,37 @@ Before ({timeout: 100 * 1000}, async function() {
     //Opens the website to the default URL in the 'protractor.config.ts' file
     await browser.get("");
 });
+
+// This will run after every scenario
+After ({timeout: 100*1000} async function(scenario) {
+
+    is (scenario.result.status === Status.FAILED) 
+    {const screenShot = await browser.takeScreenshot();
+    this. attach(screenShot, "image/png");
+    }
+});
+
+var {after, Before} = require("cucumber");
+
+// Synchronous
+Before(function(){this.count=0;
+});
+
+//Asynchronous Callback
+Before(function (testCase, callback) {
+    var world = this;
+    tmp.dir({unsafeCleanup: true}, function(error, dir) {
+        if (error) {
+            callback(error);
+        } else {
+            world.tmpDir = dir;
+            callback();
+        }
+    });
+});
+
+//Asynchronous Promise
+After(function() {
+    //Assuming this.driver is selenim webdriver
+    return this. driver.quit();
+});
